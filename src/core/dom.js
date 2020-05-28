301def60d51a089219de7807c86455af13506059
murat <56967927+kupenovm@users.noreply.github.com>
2020-05-29 02:50:58 +0600

    finish table resize

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,6 +1,6 @@
 class Dom {
   constructor(selector) {
-  // #app
+    // #app
     this.$el = typeof selector === 'string'
       ? document.querySelector(selector)
       : selector
@@ -38,6 +38,30 @@ class Dom {
       this.$el.appendChild(node)
     }
     return this
+  }
+
+  get data() {
+    return this.$el.dataset
+  }
+
+  closest(selector) {
+    return $(this.$el.closest(selector))
+  }
+
+  getCoords() {
+    return this.$el.getBoundingClientRect()
+  }
+
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector)
+  }
+
+  css(styles = {}) {
+    Object
+        .keys(styles)
+        .forEach( key => {
+          this.$el.style[key] = styles [key]
+        })
   }
 }
 
